3b057fa0384092a04cdb60d77b600c09a17b49e9
Thanapol Charoenphiphob <thacharo@Thanapols-MacBook-Pro.local>
2024-12-31 19:27:29 +0700

    A few normed in quicksort and done sort 5

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: thacharo <thacharo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/23 23:00:04 by thacharo          #+#    #+#             */
-/*   Updated: 2024/12/26 23:43:20 by thacharo         ###   ########.fr       */
+/*   Updated: 2024/12/31 19:26:31 by thacharo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,17 @@
 
 int    ft_is_sorted(t_data *data);
 void    sort_three(t_data **data);
+void    sort_five(t_data **data);
+static int    find_min_stack(t_data **data);
 void    quick_sort(t_data **data);
-void    sort_three_with_stack(t_data **data);
 
 void    ft_sort_stack(t_data **data)
 {
-    // ft_is_sorted(*data);
-    if ((*data) -> stack_a.length == 2)
+    if ((*data) -> stack_a.length <= 1)
+        return ;
+    else if (ft_is_sorted(*data))
+        return ;
+    else if ((*data) -> stack_a.length == 2)
     {
         if ((*data) -> head_a -> index > (*data) -> head_a -> next -> index)
             swap(data, "sa");
@@ -29,35 +33,65 @@ void    ft_sort_stack(t_data **data)
     else if ((*data) -> stack_a.length == 3)
         sort_three(data);
     else if ((*data) -> stack_a.length == 5)
-    {
-        sort_three_with_stack(data);
-    }
+        sort_five(data);
     else
         quick_sort(data);
-    // ft_is_sorted(*data);
     return ;   
 }
 
-void    sort_three_with_stack(t_data **data)
+void    sort_five(t_data **data)
 {
-    int top;
-    int middle;
-    int bottom;
+    int i;
+    int j;
+    int count;
     
-    top = (*data) -> head_a -> index;
-    middle = (*data) -> head_a -> next -> index;
-    bottom = (*data) -> head_a -> next -> next -> index;
-    
-    if ((top > middle && middle < bottom && top > bottom) || (top > middle && middle > bottom && top > bottom))
-        swap(data, "sa");
-    if ((top < middle && middle > bottom && top < bottom) || (top > middle && middle > bottom && top > bottom) || (top > middle && middle < bottom && top > bottom))
+    i = -1;    
+    while (++i < 2)
     {
-        rotate(data, "ra");
-        swap(data, "sa");
-        reverse_rotate(data, "rra");
+        count = find_min_stack(data);
+        j = -1;
+        if (count >= 3)
+        {
+            while (++j < ((*data) -> stack_a.length - count))
+                reverse_rotate(data, "rra");    
+        }
+        else
+        {
+            while (++j < count)
+                rotate(data, "ra");
+        }
+        push(data, "pb");
+        (*data) -> stack_a.length--;
     }
-    if (top < middle && middle > bottom && top > bottom || top > middle && middle < bottom && top < bottom || (top > middle && middle > bottom && top > bottom))
-        swap(data, "sa");
+    sort_three(data);
+    push(data, "pa");
+    push(data, "pa");
+}
+
+static int    find_min_stack(t_data **data)
+{
+    int i;
+    int count;
+    t_node *trav;
+    t_node *tmp;
+    
+    i = 1;
+    tmp = (*data) -> head_a;
+    trav = tmp -> next;
+    count = -1;
+    while (trav != NULL)
+    {
+        if (tmp -> index > trav -> index)
+        {
+            count = i;
+            tmp = trav;
+        }
+        trav = trav -> next;
+        i++;
+    }
+    if (count == -1)
+        return (0);
+    return (count);
 }
 
 void    sort_three(t_data **data)
@@ -94,9 +128,7 @@ int    ft_is_sorted(t_data *data)
         while (tmp_node2 != NULL)
         {
             if (tmp_node1 -> index > tmp_node2 -> index)
-            {
-                return (0);            
-            }
+                return (0);
             tmp_node2 = tmp_node2 -> next;
         }    
         tmp_node1 = tmp_node1 -> next;
